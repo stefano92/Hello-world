8948d158419920910fdc220da7d266825f70c49f
stefano92 <stefano496@gmail.com>
2015-10-24 00:38:16 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//Programma di esempio numero 1
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Home" << endl << "Sweet" << endl << "Home!" << endl;
+    return 0;
+}
